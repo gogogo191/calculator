27da7cad5b15240c0c5e95535e1765eec40dfd20
김용준 <gogogo191@naver.com>
2019-05-18 04:55:00 +0900

    project

---
+++ test.js
@@ -0,0 +1,63 @@
+let text = document.getElementById('text');
+const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'C', 'plusminus', 'dividing', 'multiple', 'minus', 'plus', 'equal', 'dot'];
+//[0:zero, 1:one, ... 10:c, 11:plusminus...]
+const buttons = [];
+numbers.forEach(key => {
+  buttons.push(document.getElementById(key));
+})
+
+let currentClicked = 'none';
+let sum = 0;
+let operand1 = 0;
+let operand2 = 0;
+let operator = 'plus';
+
+//계산 함수
+const calculate = (operator, operand1, operand2) => {
+  if (operator === 'plus') {
+    return operand1 + operand2;
+  }
+  if (operator === 'minus') {
+    return operand1 - operand2;
+  }
+  if (operator === 'multiple') {
+    return operand1 * operand2;
+  }
+  if (operator === 'dividing') {
+    return operand1 / operand2;
+  }
+}
+
+//C버튼 눌렀을 때 계산기가 켜집니다
+buttons[10].onclick = () => {
+  text.innerHTML = '0';
+  currentClicked = 'numclicked';
+}
+
+//등호를 눌렀을 때
+buttons[16].onclick = () => {
+  text.innerHTML = calculate(buttons[15].innerHTML, operand1, operand2).toString();
+}
+
+//숫자 눌렀을 때
+for (let i = 0; i < 10; i++) {
+  buttons[i].onclick = () => {
+    if (currentClicked === 'numclicked' && text.innerHTML !== '0') {
+      text.innerHTML += buttons[i].innerHTML;
+      operand2 = Number(text.innerHTML);
+
+    } else {
+      text.innerHTML = buttons[i].innerHTML;
+      currentClicked = 'numclicked';
+    }
+  }
+}
+
+//연산 눌렀을 때
+buttons[15].onclick = () => {
+  text.innerHTML = calculate(operator, operand1, operand2).toString();
+  operand1 = Number(text.innerHTML);
+  operator = (buttons[15].innerHTML);
+  currentClicked = 'operclicked';
+}
+
